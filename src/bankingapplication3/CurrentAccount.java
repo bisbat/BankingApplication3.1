1aad31e51be55339e5d4c569774932a424060f4f
bisbat <baitong.sae-@mail.kmutt.ac.th>
2023-05-16 20:11:22 +0700

    BankingApplication3.1

---
+++ src/bankingapplication3/CurrentAccount.java
@@ -0,0 +1,44 @@
+
+package bankingapplication3;
+
+
+public class CurrentAccount implements Account{
+    private int accountNumber;
+    private String accountName;
+    private double balance;
+    private double minimum;
+    private String accountType="CurrentAccount";
+
+    public CurrentAccount(int accountNumber, String accountName, double balance, double minimum) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = balance;
+        this.minimum = minimum;
+    }
+    
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+    public double getMinimum(){
+        return this.minimum;
+    }
+    public void setMinimum(double minimum){
+        this.minimum=minimum;
+    }
+    @Override
+    public void deposit(double amount){
+        this.balance+=amount;
+    }
+    @Override
+    public void withdraw(double amount){
+        this.balance-=amount;
+    }
+    @Override
+    public double getBalance(){
+        return this.balance;
+    }
+    @Override
+    public String getAccountType(){
+        return this.accountType;
+    }
+}
